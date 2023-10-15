34c783ee6c45bc065c26648a9b1a151de2fde69b
Rizwan Ullah KHan <poineer.k@gmail.com>
2023-10-15 12:17:30 +0500

    calendly update
    update check

--- app/pages/CalendlyButton/page.jsx
+++ app/pages/CalendlyButton/page.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 function CalendlyButton() {
   const calendlyEmbedCode = `
   <!-- Calendly inline widget begin -->
-  <div class="calendly-inline-widget bg-black" data-url="https://calendly.com/vidualize/30min" style=" min-width:320px;height:700px;"></div>
-  <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
-  <!-- Calendly inline widget end -->
+<div class="calendly-inline-widget" data-url="https://calendly.com/vidualizestudio/30min" style="min-width:320px;height:700px;"></div>
+<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+<!-- Calendly inline widget end -->
   `;
 
   return (
